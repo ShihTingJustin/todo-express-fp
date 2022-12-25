43720f08f8573c548bab038710851034d08c11b6
ShihTingJustin <justinhuang777@gmail.com>
2022-12-25 18:15:57 +0800

    chore: import cors and dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,20 @@
 import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
 import bodyParser from 'body-parser';
 import allRouter from './routes';
 
 const app: Express = express();
 const PORT = process.env.PORT || 3000;
 
+require('dotenv').config();
 require('./config/mongoose');
 
+app.use(
+  cors({
+    origin: `${process.env.CLIENT_DOMAIN}`,
+    optionsSuccessStatus: 200,
+  }),
+);
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(allRouter);
