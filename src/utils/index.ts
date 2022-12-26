f976ea0b7d542fa142313c0b7b146ea08b62b990
ShihTingJustin <justinhuang777@gmail.com>
2022-12-26 16:09:28 +0800

    feat: add updateTodoByTodoId utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
 import List from '@Models/list';
-import Todo from '@Models/todo';
+import Todo, { MTodo } from '@Models/todo';
 import { ITodo, CreateTodoReqBody } from '@Interfaces/I_todo';
 
 export async function checkListIdValid(listId: string) {
@@ -14,6 +14,23 @@ export async function checkListIdValid(listId: string) {
 export async function addTodo(todo: CreateTodoReqBody) {
   try {
     const data = await Todo.create(todo);
+    const newTodo: ITodo = {
+      id: data._id,
+      title: data.title,
+      status: data.status,
+      listId: data.listId,
+    };
+    return newTodo;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function updateTodoByTodoId(todoId: ITodo['id'], update: Partial<MTodo>) {
+  try {
+    const data = await Todo.findByIdAndUpdate(todoId, update, { new: true });
+    if (!data) return false;
+
     const newTodo: ITodo = {
       id: data._id,
       title: data.title,
