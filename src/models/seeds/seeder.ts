8cd80cecf007a0e6d36f2ba2716639b57b8367a8
ShihTingJustin <justinhuang777@gmail.com>
2022-12-28 21:24:23 +0800

    chore: update seeder

--- src/models/seeds/seeder.ts
+++ src/models/seeds/seeder.ts
@@ -1,10 +1,29 @@
+import { faker } from '@faker-js/faker';
+import List from '@Models/list';
 import Todo from '@Models/todo';
 import db from '@Config/mongoose';
+import { TodoStatus } from '@Interfaces/I_todo';
+
+const mockList = ['Reminders', 'Grocery List', 'Habits', 'Family', 'Emergency'];
 
 db.once('open', async () => {
   try {
     console.log('run seeder...');
-    await Todo.create({ title: 'test', completed: false });
+
+    const listRes = await List.create(mockList.map((list) => ({ title: list })));
+    const mockTodo = listRes
+      .map((list, index) =>
+        Array.from({ length: index }, () => ({
+          listId: list._id,
+          title: faker.word.noun(),
+          status: TodoStatus.UNFINISH,
+          isDelete: false,
+        })),
+      )
+      .flat();
+
+    await Todo.create(mockTodo);
+
     console.log('seeder complete!');
     process.exit();
   } catch (err) {
