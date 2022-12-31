914ae8cc577d25f513d67a0fb4de285b62d02276
ShihTingJustin <justinhuang777@gmail.com>
2023-01-01 01:54:15 +0800

    feat: add todoEntity and userEntity

---
+++ src/entities/todoEntity.ts
@@ -0,0 +1,42 @@
+import List from '@Models/list';
+import Todo, { ITodo } from '@Models/todo';
+
+export const getListByListId = (listId: string) => {
+  return List.findById(listId).lean();
+};
+
+export const createTodo = (todo: ITodo) => {
+  return Todo.create(todo);
+};
+
+export const updateTodo = (todo: ITodo) => {
+  return Todo.findByIdAndUpdate(todo.id, todo);
+};
+
+export const deleteTodo = (todoId: ITodo['id']) => {
+  return Todo.findByIdAndUpdate(todoId, { isDeleted: true });
+};
+
+export const getTodoAmountByListId = ({
+  listId,
+  isDelete = false,
+}: {
+  listId: string;
+  isDelete?: boolean;
+}) => {
+  return Todo.countDocuments({ listId, isDelete }).lean();
+};
+
+export const searchTodoByFilter = ({ keyword }: { keyword: string }) => {
+  return Todo.find(
+    {
+      $or: [{ title: { $regex: new RegExp(keyword, 'i') } }],
+      $and: [{ isDelete: false }],
+    },
+    {},
+    {
+      sort: { _id: 1 },
+      limit: 10,
+    },
+  );
+};
---
+++ src/entities/userEntity.ts
@@ -0,0 +1,15 @@
+import User from '@Models/user';
+
+export const findListAndTodoFromUser = () => {
+  return User.findOne()
+    .populate({
+      path: 'lists',
+      select: '-owner -__v -createdAt -updatedAt',
+      populate: {
+        path: 'todos',
+        select: '-isDeleted -listId -__v -createdAt -updatedAt',
+      },
+    })
+    .select('-__v -createdAt -updatedAt')
+    .lean();
+};
