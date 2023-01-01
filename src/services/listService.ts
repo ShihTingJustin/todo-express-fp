ef3f8b81981bd164101d909075c103afd1b66c93
ShihTingJustin <justinhuang777@gmail.com>
2023-01-01 18:58:04 +0800

    feat: add listService

---
+++ src/services/listService.ts
@@ -0,0 +1,21 @@
+import { findListByListId } from '@Entities/listEntity';
+import { IList } from '@Models/list';
+
+export const getListByListId = async (listId: string) => {
+  try {
+    const data = await findListByListId(listId);
+    if (data) {
+      const { _id, title, todos } = data as unknown as IList;
+      return {
+        id: _id,
+        title,
+        todos: todos.map((todo) => {
+          const { _id, ...rest } = todo;
+          return { id: todo._id, ...rest };
+        }),
+      };
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
