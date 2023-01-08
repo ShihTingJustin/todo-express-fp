4fcef9825766716d1898e5b44d05fe6d9b6c2877
ShihTingJustin <justinhuang777@gmail.com>
2023-01-08 13:06:49 +0800

    feat: add validator and sanitizer option and middleware

---
+++ src/validators/todoValidator.ts
@@ -0,0 +1,55 @@
+import { Schema } from 'express-validator';
+
+export const todoSchemaValidateOption: { [key: string]: Schema } = {
+  searchTodo: {
+    keyword: {
+      in: 'params',
+      trim: true,
+      escape: true,
+      errorMessage: 'keyword is wrong',
+    },
+  },
+  createTodo: {
+    listId: {
+      in: 'body',
+      isMongoId: true,
+      errorMessage: 'listId is wrong',
+    },
+    title: {
+      in: 'body',
+      isString: true,
+      escape: true,
+      errorMessage: 'title is wrong',
+    },
+    completed: {
+      in: 'body',
+      isBoolean: true,
+      errorMessage: 'completed is wrong',
+    },
+  },
+  updateTodo: {
+    id: {
+      in: 'body',
+      isMongoId: true,
+      errorMessage: 'id is wrong',
+    },
+    title: {
+      in: 'body',
+      isString: true,
+      escape: true,
+      errorMessage: 'title is wrong',
+    },
+    completed: {
+      in: 'body',
+      isBoolean: true,
+      errorMessage: 'completed is wrong',
+    },
+  },
+  deleteTodo: {
+    todoId: {
+      in: 'params',
+      isMongoId: true,
+      errorMessage: 'todoId is wrong',
+    },
+  },
+};
