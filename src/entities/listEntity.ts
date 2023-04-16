a3b947270c86aaecfcb5c06846ec40af6ae2a055
ShihTingJustin <justinhuang777@gmail.com>
2023-04-16 23:58:45 +0800

    feat: add sorter for todos query

--- src/entities/listEntity.ts
+++ src/entities/listEntity.ts
@@ -6,7 +6,13 @@ export const findListByListId = (listId: string) => {
   })
     .populate({
       path: 'todos',
-      select: '-isDeleted -listId -__v -createdAt -updatedAt',
+      select: '-isDeleted -listId -__v -createdAt',
+      options: {
+        sort: {
+          completed: 1, // 將 completed: true 的待辦事項排在前面
+          updatedAt: -1, // 根據 updatedAt 降序排列
+        },
+      },
     })
     .select('-owner -__v -createdAt -updatedAt')
     .lean();
--- src/entities/userEntity.ts
+++ src/entities/userEntity.ts
@@ -9,7 +9,13 @@ export const findListAndTodoFromUser: () => Promise<IUserDocument> = async () =>
       select: '-owner -__v -createdAt -updatedAt',
       populate: {
         path: 'todos',
-        select: '-isDeleted -listId -__v -createdAt -updatedAt',
+        select: '-isDeleted -listId -__v -createdAt',
+        options: {
+          sort: {
+            completed: 1, // 將 completed: true 的待辦事項排在前面
+            updatedAt: -1, // 根據 updatedAt 降序排列
+          },
+        },
       },
     })
     .select('-__v -createdAt -updatedAt')
