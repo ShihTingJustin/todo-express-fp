430b18031fd0298d9d12767cc5cb04f4198fe173
ShihTingJustin <justinhuang777@gmail.com>
2023-04-16 22:01:36 +0800

    feat: add health route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 import swaggerJsDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
@@ -12,5 +12,29 @@ const allRouter = express.Router();
 allRouter.use('/todo', todoRouter);
 allRouter.use('/list', listRouter);
 allRouter.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJsDoc(swaggerConfig)));
+allRouter.get('/health', async (_req: Request, res: Response) => {
+  const healthCheck = {
+    message: 'OK',
+    uptime: formatUptime(process.uptime()),
+    timestamp: Date.now(),
+  };
+  try {
+    res.status(200).send(healthCheck);
+  } catch (error) {
+    healthCheck.message = String(error);
+    res.status(503).send('something went wrong');
+  }
+});
+
+function formatUptime(uptime: number) {
+  const days = Math.floor(uptime / 86400);
+  uptime %= 86400;
+  const hours = Math.floor(uptime / 3600);
+  uptime %= 3600;
+  const minutes = Math.floor(uptime / 60);
+  const seconds = Math.floor(uptime % 60);
+
+  return `${days}天 ${hours}小時 ${minutes}分鐘 ${seconds}秒`;
+}
 
 export default allRouter;
